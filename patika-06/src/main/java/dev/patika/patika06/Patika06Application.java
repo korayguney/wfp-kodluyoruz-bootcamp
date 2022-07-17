140bcec92c612b5f08ef1740f0bfe19d4d4e8371
Koray Guney <koray.guney@hotmail.com>
2022-07-17 14:47:02 +0300

    seventh commit

---
+++ patika-06/src/main/java/dev/patika/patika06/Patika06Application.java
@@ -0,0 +1,13 @@
+package dev.patika.patika06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Patika06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Patika06Application.class, args);
+    }
+
+}
