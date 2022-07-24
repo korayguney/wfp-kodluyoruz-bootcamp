6ef062bebff663bc8e5ff2a7eb9bccda9c83ec58
Koray Guney <koray.guney@hotmail.com>
2022-07-24 11:25:03 +0300

    last week commit

--- patika-05/src/test/java/dev/patika/patika05/service/EmployeeServiceImplTest.java
+++ patika-05/src/test/java/dev/patika/patika05/service/EmployeeServiceImplTest.java
@@ -1,5 +1,6 @@
 package dev.patika.patika05.service;
 
+import com.fasterxml.jackson.databind.util.ArrayIterator;
 import dev.patika.patika05.model.Employee;
 import dev.patika.patika05.repository.EmployeeRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -12,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,18 +28,24 @@ class EmployeeServiceImplTest {
     EmployeeServiceImpl employeeService;
 
 
-
     @Test
     void findAllTest() {
+        // given
+        Employee employee1 = new Employee();
+        employee1.setSalary(25000);
+        Employee employee2 = new Employee();
+        List<Employee> employees = new ArrayList<>();
+        employees.add(employee1);
+        employees.add(employee2);
+        Mockito.when(repository.findAll()).thenReturn(employees);
 
-      //  // given
-      //  Mockito.when(repository.findAll()).thenReturn(Mockito.anyIterable());
-//
-      //  // when
-      //  List<Employee> expected = employeeService.findAll();
-//
-      //  // then
-      //  Assertions.assertNotNull(expected);
+        // when
+        List<Employee> expected = employeeService.findAll();
+
+        // then
+        Assertions.assertNotNull(expected);
+        Assertions.assertEquals(employees.size(), 2);
+        Assertions.assertTrue(expected.get(0).getSalary() > 20000);
     }
 
 }
